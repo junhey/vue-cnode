45e8b62ea35b1ff259f81bf5b1ba03e874f992bd
junhey <hejun@modernavenue.com>
2017-06-15 09:45:23 +0800

    增加文件夹

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import $ from 'webpack-zepto';
+import VueRouter from 'vue-router';
+import FastClick from 'fastclick';
+import Alert from './libs/alert';
+
+Vue.use(VueRouter);
+Vue.use(Alert);
+
+$.ajaxSettings.crossDomain = true;
 
 Vue.config.productionTip = false
 
