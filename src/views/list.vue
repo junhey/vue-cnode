2f3a6399d69539b7e583951cd66a8b1eec1a6761
junhey <junhey@qq.com>
2017-07-07 14:10:54 +0800

    测试axios成功

--- src/views/list.vue
+++ src/views/list.vue
@@ -1,17 +1,41 @@
 <template>
     <div>
-        <div class="index">list<p>list</p></div>
+        <section id="list-page">
+            <button v-on:click="getList">list</button><br>{{msg}}---{{data}}
+        </section>
     </div>
 </template>
 <script>
-    
-</script>
-<style lang="scss" scoped>
-    .index {
-        width: 100%;
-        background-color: #fff;
-        p{
-            font-size: 100px;
+    import axios from 'axios'
+    export default{
+        data(){
+            return {
+                msg:'hello axios',
+                data:[]
+            }
+        },
+        components:{
+
+        },
+        methods:{
+            getList(){
+                let _this=this;
+                axios.get('https://cnodejs.org/api/v1/topics',{
+                    params:{
+                        page:1,
+                        limit:20,
+                        tab:'all',
+                        mdrender:true
+                    }
+                }).then(function(res){
+                    console.log(res);
+                    console.log(_this);
+                    _this.data=res;
+                });
+            }
         }
     }
+</script>
+<style lang="scss" scoped>
+    
 </style>
