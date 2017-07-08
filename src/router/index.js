59e43fdf8fe5ea9d4783ca70b7984ac56da575bf
junhey <junhey@qq.com>
2017-07-08 22:14:59 +0800

    增加路由，默认访问list

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,11 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
+      path:'/',
+      name:'list',
+      component: list
+    },
+    {
       path:'/list',
       name:'list',
       component: list
